c77050f00253a6e61ae3bd17326611571b0657be
Jonas <jonaspaq@gmail.com>
2021-08-18 16:42:46 +0800

    Update router

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,10 +5,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: '/me/'
-  },
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'me',
