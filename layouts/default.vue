b4250c89745f7c93a0a47a0b2dbe1d0a72e75999
Jonas <jonaspaq@gmail.com>
2021-10-04 16:24:44 +0800

    Update coming soon page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="main-container flex">
-    <Nuxt />
+  <div class="h-screen flex flex-col bg-black relative px-1">
+    <div class="fixed w-full z-10">
+      <nav class="container mx-auto flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 shadow sm:items-baseline w-full bg-transparent">
+        <div class="mb-2 sm:mb-0">
+          <a href="/" class="text-xl no-underline text-gray-400 bg-opacity-90 hover:text-blue-dark">Jonas Paquibot</a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/jonaspaq/" target="_blank" class="text-sm no-underline text-gray-400 bg-opacity-90 hover:text-white ml-2">LinkedIn</a>
+          <a href="https://github.com/jonaspaq" target="_blank" class="text-sm no-underline text-gray-400 bg-opacity-90 hover:text-white ml-2">Github</a>
+        </div>
+      </nav>
+    </div>
+
+    <Nuxt class="h-full" />
   </div>
 </template>
-
-<style lang="scss" scoped>
-.main-container {
-  height: 100vh;
-  background-color: rgb(13, 13, 13);
-}
-</style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container mx-auto flex flex-col items-center justify-center">
     <h1 class="text-white mx-auto text-7xl">
-      404 Page not found. 🐶
+      404 Page not found 🧩
     </h1>
-    <NuxtLink to="/" class="text-white mx-auto mt-4">
+    <NuxtLink to="/" class="text-white mx-auto mt-4 underline">
       Go back to homepage
     </NuxtLink>
   </div>
